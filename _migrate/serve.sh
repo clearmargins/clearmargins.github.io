1178244f441c2468a54ad5a6d91d3b6a35f193cd
Sue Smith <sue@benormal.info>
2026-04-15 17:58:47 +0100

    Delete _migrate directory

--- _migrate/serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script spins up a local test Compute app
-
-npm run build 
-npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=website-content 
-cd compute-js 
-npm run dev:publish 
-npm run dev:start
